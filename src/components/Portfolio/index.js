c61b0392fdff814a2949db01b4f09750d2f11869
Michelle Nijjar <michellenijjar@hotmail.com>
2022-01-25 04:26:00 -0500

    added new links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import photo1 from '../../assets/img/employeetracker.png';
 import photo2 from '../../assets/img/learningporch.png';
 import photo3 from '../../assets/img/notetaker.png';
-import photo4 from '../../assets/img/weather-dashboard.png';
+import photo4 from '../../assets/img/wellnesshub.png';
 
 const PhotoList = () => {
 
@@ -18,7 +18,7 @@ const PhotoList = () => {
             className="img-thumbnail mx-1"
           />
           </a>
-          <a href="https://github.com/ToiletTakos/learning-porch">
+          <a href="https://the-learning-porch.herokuapp.com/">
             <p>Learning Porch</p>
           <img
             src={photo2}
@@ -26,7 +26,7 @@ const PhotoList = () => {
             className="img-thumbnail mx-1"
           />
           </a>
-          <a href="https://github.com/mnijjar24/note-taker">
+          <a href="https://mnijjar24.github.io/note-taker/">
             <p>Note Taker</p>
           <img
             src={photo3}
@@ -34,8 +34,8 @@ const PhotoList = () => {
             className="img-thumbnail mx-1"
           />
           </a>
-          <a href="https://github.com/mnijjar24/weather-dashboard">
-            <p>Weather Dashboard</p>
+          <a href="https://nnikhil84.github.io/wellnesshub.github.io">
+            <p>Wellness Hub</p>
           <img
             src={photo4}
             alt='afdsf'
